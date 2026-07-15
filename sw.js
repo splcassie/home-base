99c3726773c6cd0a9347ac907782f7e04231b430
Cassie <39582637+splcassie@users.noreply.github.com>
2026-07-15 11:01:07 +0200

    Missed file

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cassie-home-base-v1';
+const CACHE_NAME = 'cassie-home-base-v2';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
