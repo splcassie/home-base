b6eaa711d3591a464779865af805d0a4fdcbf251
Cassie <39582637+splcassie@users.noreply.github.com>
2026-07-25 12:10:09 +0200

    Added due date, daily progress bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cassie-home-base-v6';
+const CACHE_NAME = 'cassie-home-base-v7';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
