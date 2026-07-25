dc46d5c4a8162b89c484348943bc938f3cd711e0
Cassie <39582637+splcassie@users.noreply.github.com>
2026-07-25 13:49:17 +0200

    added stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cassie-home-base-v7';
+const CACHE_NAME = 'cassie-home-base-v8';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
